e8123676699055793d134b0a4586f52411a2295a
Emmanuel <emmanuelfleurine@gmail.com>
2018-05-31 15:19:44 -0400

    Upgrade android development tools
    This commit upgrade different android development tools
    it also change the diplay app name to a more concise "Chantons"

--- android/app/src/main/java/com/sakcho/chantons/MainActivity.java
+++ android/app/src/main/java/com/sakcho/chantons/MainActivity.java
@@ -3,23 +3,24 @@ package com.sakcho.chantons;
 import android.os.Bundle;
 
 import com.facebook.react.ReactActivity;
-import com.reactnativecomponent.splashscreen.RCTSplashScreen;   
+import com.reactnativecomponent.splashscreen.RCTSplashScreen;
 
 public class MainActivity extends ReactActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        RCTSplashScreen.openSplashScreen(this);   //open splashscreen
-        //RCTSplashScreen.openSplashScreen(this, true, ImageView.ScaleType.FIT_XY);   //open splashscreen fullscreen
+        RCTSplashScreen.openSplashScreen(this); // open splashscreen
+        // RCTSplashScreen.openSplashScreen(this, true, ImageView.ScaleType.FIT_XY);
+        // //open splashscreen fullscreen
         super.onCreate(savedInstanceState);
     }
 
     /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
+     * Returns the name of the main component registered from JavaScript. This is
+     * used to schedule rendering of the component.
      */
     @Override
     protected String getMainComponentName() {
-        return "ChantonsavecEsperance";
+        return "Chantons";
     }
 }
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import { AppRegistry } from "react-native";
+import App from "./App";
 
-AppRegistry.registerComponent('ChantonsavecEsperance', () => App);
+AppRegistry.registerComponent("Chantons", () => App);
