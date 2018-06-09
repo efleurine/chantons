870145aa9fe13c7485dc4393648ec9629d696b6f
Emmanuel <emmanuelfleurine@gmail.com>
2018-06-08 23:34:45 -0400

    Add an index to the Components
    In other to easily reference components

---
+++ src/Components/index.js
@@ -0,0 +1,15 @@
+import Container from "./Container";
+import EmailInput from "./EmailInput";
+import PasswordInput from "./PasswordInput";
+import Text from "./Text";
+import ResetPassword from "./ResetPassword";
+import NumberInput from "./NumberInput";
+
+export {
+  Container,
+  EmailInput,
+  PasswordInput,
+  Text,
+  ResetPassword,
+  NumberInput
+};
