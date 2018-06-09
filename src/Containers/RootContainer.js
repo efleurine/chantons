1e7783986f2ed76977f941410f12a99ec8ea83df
Emmanuel <emmanuelfleurine@gmail.com>
2018-06-08 23:39:47 -0400

    Add a root container
    This is the first visual element. Following Chain React App best practices

--- src/Containers/RootContainer.js
+++ src/Containers/RootContainer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import AppNavigation from "../Navigations/AppNavigation";
-import TestScreen from "../Containers/TestScreen";
+// import TestScreen from "../Containers/TestScreen";
 
 export default class RootContainer extends React.Component {
   componentDidMount() {
