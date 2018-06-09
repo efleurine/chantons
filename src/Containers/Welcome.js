5dd7d09a151b819152007fc85ceea9395b353083
Emmanuel <emmanuelfleurine@gmail.com>
2018-06-08 23:32:59 -0400

    Add a welcome screen
    This will be the first launched after the inital installation

--- src/Containers/Welcome.js
+++ src/Containers/Welcome.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
+import PropTypes from "prop-types";
 
 class WelcomeScreen extends Component {
   static navigationOptions = {
     title: "Welcome"
   };
 
+  static propTypes = {
+    navigation: PropTypes.object.isRequired
+  };
+
   componentDidMount() {
     console.log("First time screen is mounted");
+    setTimeout(() => this.props.navigation.navigate("Registration"), 2000);
   }
 
   render() {
